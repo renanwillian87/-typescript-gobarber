6539f90c0598acb1bb3ca1f530c4394730205bd4
renanwillian87 <renan.santos@remessaonline.com.br>
2021-03-14 22:36:32 -0300

    chore: add midleware celebrate to valid requests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors'
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -18,10 +19,12 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if(err instanceof AppError) {
         return response.status(err.statusCode).json({
-            statue: 'error',
+            status: 'error',
             message: err.message
         })
     }
