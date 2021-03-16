33532c4ab903b6745d7456e5ba415e850d5ea1ae
renanwillian87 <renan.santos@remessaonline.com.br>
2021-03-15 22:11:43 -0300

    chore: implement aws ses email service

---
+++ src/shared/providers/MailProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+import IMailProvider from './models/IMailProvider';
+import EtherealMailProvider from  './implementations/EtherealMailProvider';
+import AWSSESMailProvider from  './implementations/AWSSESMailProvider';
+
+const providers = {
+    ethereal: container.resolve(EtherealMailProvider),
+    ses: container.resolve(AWSSESMailProvider)
+}
+
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    providers[mailConfig.driver]
+);
