39e04db83f49925e68b9806bf427233750ea5383
renanwillian87 <renan.santos@remessaonline.com.br>
2021-03-16 22:46:09 -0300

    chore: create provider to redis cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/providers/CacheProvider/models/ICacheProvider';
 interface IRequest {
     provider_id: string;
     day: number;
@@ -11,15 +12,21 @@ interface IRequest {
 class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
+        private appointmentsRepository: IAppointmentsRepository,
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider
     ) {}
     public async execute({ provider_id, year, month, day }: IRequest) : Promise<Appointment[]> {
+        // const cacheData = await this.cacheProvider.recover('dasd');
+        // console.log(cacheData);
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id,
             year,
             month,
             day
         });
+        // if(cacheData === null)
+        // await this.cacheProvider.save('dasd', 'asd');
         return appointments;
     }
 }
--- src/shared/providers/MailProvider/index.ts
+++ src/shared/providers/MailProvider/index.ts
@@ -3,12 +3,10 @@ import mailConfig from '@config/mail';
 import IMailProvider from './models/IMailProvider';
 import EtherealMailProvider from  './implementations/EtherealMailProvider';
 import AWSSESMailProvider from  './implementations/AWSSESMailProvider';
-
 const providers = {
     ethereal: container.resolve(EtherealMailProvider),
     ses: container.resolve(AWSSESMailProvider)
 }
-
 container.registerInstance<IMailProvider>(
     'MailProvider',
     providers[mailConfig.driver]
